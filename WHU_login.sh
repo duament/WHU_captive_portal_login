1101662f85126a044f042dc3633c613918e760e1
duament <30264485+duament@users.noreply.github.com>
2019-12-17 22:34:47 +0800

    Update User-Agent

--- WHU_login.sh
+++ WHU_login.sh
@@ -3,7 +3,7 @@ unset http_proxy
 unset https_proxy
 DETECT_URL='http://conn1.oppomobile.com/generate_204'
 LOGIN_POST_URL='http://172.19.1.9:8080/eportal/InterFace.do?method=login'
-UA='Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
+UA='Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0'
 TYPE='Internet'
 
 help() {
